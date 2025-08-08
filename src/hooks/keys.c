d72c94d237a3e8def4391bcac435e7a64b15f627
Amine Elsayed <elsayedamine1337@gmail.com>
2025-08-08 18:18:01 +0100

    working on translation

--- src/hooks/keys.c
+++ src/hooks/keys.c
@@ -6,7 +6,7 @@
 /*   By: sayed <sayed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 16:38:23 by sayed             #+#    #+#             */
-/*   Updated: 2025/08/08 17:17:43 by sayed            ###   ########.fr       */
+/*   Updated: 2025/08/08 18:16:56 by sayed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int keyhook(int key, t_minirt *vars)
 {
 	if (key == 65307)
 		quit(vars);
-	// if (ft_strchr("wasdqe", key) && vars->selected)
-	// 	translation(vars);
+	if (ft_strchr("wasdqe", key) && vars->selected == 1)
+		translation(vars, key);
+	if (ft_strchr("wasdqe", key) && vars->selected == 2)
+		rotation(vars, key);
     return (TRUE);
 }
 
